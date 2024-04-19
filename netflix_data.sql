b1f359a890392960d808dd3c64b558166fda1b6e
Chuchu Wang <chuchu@chuchus-air.mynetworksettings.com>
2024-04-18 21:07:49 -0500

    pull

---
+++ netflix_data.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Netflix_data (
+    latitude FLOAT,
+    longitude FLOAT,
+    country VARCHAR(255),
+    type VARCHAR(255),
+    title VARCHAR(255),
+    director VARCHAR(255),
+    date_added Varchar,
+    release_year INT,
+    rating VARCHAR(255),
+    duration VARCHAR(255),
+    listed_in VARCHAR(255),
+    country_count INT
+);
+
+select * from Netflix_data;
